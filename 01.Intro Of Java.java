80d6152a2b607797bb9749d418c529bfdc7282ae
Cap-Panaka <nerdcrab1122@gmail.com>
2025-11-25 22:51:11 +0600

    Update 01.Intro Of Java.java

--- 01.Intro Of Java.java
+++ 01.Intro Of Java.java
@@ -1,5 +1,23 @@
-public class Main {
+01. public class Main {
     public static void main(String[] args) {
      // System.out.println("Hello World");
     }
+}
+
+
+02. 
+    public class Main {
+    public static void main(String[] args) {
+     System.out.println("I like pizza\n");
+     System.out.println("It's really good\n");
+     System.out.println("Buy me a pizza");
+    }
+    
+    /*
+    I like pizza
+
+It's really good
+
+Buy me a pizza
+    */
 }

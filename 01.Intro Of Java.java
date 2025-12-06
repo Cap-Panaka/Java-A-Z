155ec7959276f7ea746647a2ee9dd33e4d69babe
Cap-Panaka <nerdcrab1122@gmail.com>
2025-12-06 11:18:10 +0600

    Update 01.Intro Of Java.java

--- 01.Intro Of Java.java
+++ 01.Intro Of Java.java
@@ -1,16 +1,28 @@
 public class Main {
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // THIS IS MY FIRST JAVA PROGRAM
-     System.out.println("I like pizza");
-     System.out.println("It's really good");
-     System.out.println("Buy me a pizza");
+
+        /*
+
+    This is  a multi-line
+    comment
+
+    */
+
+        System.out.print("I like pizza!\n");
+        System.out.print("It's a really good\n");
+        System.out.print("Buy me a pizza!");
+
+
+        // sout + tab = System.out.println();
+
     }
 
-    /*
-    I like pizza
-It's really good
-Buy me a pizza
-    */
+
 }
+
+//I like pizza!
+//It's a really good
+//Buy me a pizza!
 

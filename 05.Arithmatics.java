3fa1122732a3de834be935300c9a7ea588d3d75a
Cap-Panaka <nerdcrab1122@gmail.com>
2025-12-09 01:31:49 +0600

    Create 05.Arithmatics.java

---
+++ 05.Arithmatics.java
@@ -0,0 +1,75 @@
+
+//public class Main{
+//    1. public static void main(String[] args)
+//    {
+//        //Arithmetic operations
+//
+//
+//        int x = 10;
+//        int y = 2;
+//        int z = 0;
+//
+//        // z=x+y; //12
+//        //z=x-y; //8
+//       // z= x * y; //20
+//       // z = x / y ; //5
+//       // z = x % y ; //0
+//
+//
+//
+//
+//        System.out.println(z);
+//
+//    }
+//
+//}
+
+
+//2.
+//public class Main
+//{
+//    public static void main(String[] args)
+//    {
+//        int x=10;
+//        int y=3;
+//        //Augmented Assignment Operators
+//        //x+=y;  //13
+//        //x-=y; //7
+//        //x*=y; //30
+//        //x/=y; //3
+//
+//        // x%=y; //1
+//
+//
+//        System.out.println(x);
+//    }
+//
+//}
+
+//3.
+//public class Main
+//{
+//    public static void  main(String[] args)
+//    {
+//        // Increment and Decrement Operators
+//
+//        int x = 1;
+//        x++;
+//        x++;
+//        x--;//2
+//
+//        System.out.println(x);
+//    }
+//}
+
+
+public class Main {
+    public static void main(String[] args)
+    {
+         //ORDER OF OPERATIONS [PEMDAS]
+
+        double result = 3 + 4 * (7-5) / 2.0 ; //7.0
+
+        System.out.println(result);
+    }
+}

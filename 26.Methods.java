d79c21a17be7ede522734e54e7ca7125d0f82b2e
Cap-Panaka <nerdcrab1122@gmail.com>
2025-12-13 19:44:47 +0600

    Create 26.Methods.java

---
+++ 26.Methods.java
@@ -0,0 +1,75 @@
+ public class rick {
+    public static void main(String[] args){
+
+         // method =  a block of reusable code that is executed when called ()
+		 
+		 int age= 20;
+		 String name = "Spongebob";
+		 
+		 HappyBirthday(age,name);
+		 
+		 
+    }
+	static void HappyBirthday(int yr, String name)
+	{
+		System.out.println("Happy birthday to you!");
+		System.out.printf("Happy Birthday Dear %s\n",name);
+		System.out.printf("You are %d years old\n",yr);
+		System.out.println("Happy Birthday to you!\n");
+		
+	}
+	
+	/*Happy birthday to you!
+Happy Birthday Dear Spongebob
+You are 20 years old
+Happy Birthday to you! */
+
+
+   2.
+    //retrun 
+ public class rick {
+    public static void main(String[] args){
+		double res = square(3);
+		System.out.print(res);
+		
+
+	
+	}
+	static double square(double num)
+	{
+		
+	   return 	num * num ;
+	}
+	//9.0
+	
+
+
+	
+ }
+
+   3. 
+
+   
+ public class rick {
+    public static void main(String[] args){
+	double x = 3;
+	System.out.println(cube(3));
+
+
+	}
+	
+	static double cube(double number){
+	return number*number*number;
+	}
+	//27
+ }
+
+
+   
+
+
+   
+
+	
+	
+}

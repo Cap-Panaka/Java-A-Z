b5fa0e54c9b8787049b7452ecd9b9da9d6ced772
Cap-Panaka <nerdcrab1122@gmail.com>
2026-03-15 23:21:10 +0600

    Implement file writing functionality in Java

---
+++ 58.WriteFiles.java
@@ -0,0 +1,39 @@
+//import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public  class Main{
+    public static void main(String[] args){
+        //How to write a file using Java(4 popular options)
+
+        //FileWriter = Good for small or medium-sized text files
+        //BufferedWriter = Better performance for large amounts of text
+        //PrintWriter = Best for structured data , like reports or logs
+        //FileOutputStream = Best for binary files (e.g. , images , audio files)
+
+        String filePath = "C:\\Users\\USER\\OneDrive\\Desktop\\test.txt";
+       // String textContent = "I like pizza!\nIt's really good!\nBuy me pizza!";
+
+        String textContent = """
+                Roses are Red
+                Violets are blue
+                BOTTY BOOTY
+                ROCKIN' EVERYWHERE!
+                """;
+
+        try(FileWriter writer = new FileWriter(filePath)){
+            writer.write(textContent);
+            System.out.println("File has been written");
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        catch(IOException e){
+            System.out.println("Could not write file");
+
+        }
+
+
+    }
+}

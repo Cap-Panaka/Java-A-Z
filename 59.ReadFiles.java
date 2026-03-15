5389dcca1c642089ecd8194a4b60128bfae72e72
Cap-Panaka <nerdcrab1122@gmail.com>
2026-03-15 23:31:30 +0600

    Create 59.ReadFiles.java

---
+++ 59.ReadFiles.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //How to read a file using JAVA (3 popular options)
+
+        //BufferREADER + FileReader: Best for reading text files line-by-line
+        //FileINputStream: Best for binary files(e.g. images, audio files)
+        // RandomAccessFile: Best for read/write specific portions of a large file
+
+
+        String filePath ="C:\\Users\\USER\\OneDrive\\Desktop\\test.txt";
+
+
+
+        try(BufferedReader reader = new  BufferedReader(new FileReader(filePath))){
+
+            String line;
+            while( (line = reader.readLine()) != null){
+                System.out.println(line);
+            }
+            //System.out.println("That file exists");
+
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Something went wrong");
+        }
+
+    }
+}
